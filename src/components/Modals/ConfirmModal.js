566df1c34bffdc3e59be96fc82ed4350e033ab24
Dzenan Košuta <dzenan_kosuta@hotmail.com>
2024-06-04 15:11:38 +0200

    add confirm delete user modal

--- src/components/Modals/ConfirmModal.js
+++ src/components/Modals/ConfirmModal.js
@@ -1,11 +1,11 @@
-import { View, Text, TouchableOpacity } from 'react-native';
-import React from 'react';
-import Modal from 'react-native-modal';
-import { useTheme } from '@react-navigation/native';
-import { useTranslation } from 'react-i18next';
-import Feather from 'react-native-vector-icons/Feather';
-import { moderateScale } from 'react-native-size-matters';
-import styles from './ConfirmModalStyles';
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import Modal from "react-native-modal";
+import { useTheme } from "@react-navigation/native";
+import { useTranslation } from "react-i18next";
+import Feather from "react-native-vector-icons/Feather";
+import { moderateScale } from "react-native-size-matters";
+import styles from "./ConfirmModalStyles";
 
 const ConfirmModal = ({
   modalVisible,
@@ -20,14 +20,14 @@ const ConfirmModal = ({
     <Modal
       isVisible={modalVisible}
       backdropOpacity={0.5}
-      // onBackdropPress={() => setModalVisible(false)}
+      onBackdropPress={() => setModalVisible(false)}
     >
       <View
         style={[styles.modalContainer, { backgroundColor: colors.background }]}
       >
         <View
           style={{
-            position: 'absolute',
+            position: "absolute",
             top: moderateScale(20, 0.2),
             left: moderateScale(20, 0.2),
           }}
@@ -37,16 +37,26 @@ const ConfirmModal = ({
         <Text style={[styles.modalTitle, { color: colors.text }]}>{title}</Text>
 
         <View style={[styles.infoContainer, { borderTopColor: colors.border }]}>
-          <Text style={{ color: colors.text }}>{info}</Text>
+          <Text style={{ color: colors.text, textAlign: "center" }}>
+            {info}
+          </Text>
         </View>
         <View style={[styles.btnContainer]}>
           <TouchableOpacity
             activeOpacity={0.8}
-            underlayColor={'transparent'}
-            style={[styles.btn, { backgroundColor: colors.primary }]}
+            underlayColor={"transparent"}
+            style={[styles.btn, { backgroundColor: colors.dangerous }]}
             onPress={confirm}
           >
-            <Text style={{ color: colors.border }}>{t('ok')}</Text>
+            <Text style={{ color: colors.link }}>{t("yes")}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            activeOpacity={0.8}
+            underlayColor={"transparent"}
+            style={[styles.btn, { backgroundColor: colors.primary }]}
+            onPress={() => setModalVisible(false)}
+          >
+            <Text style={{ color: colors.link }}>{t("no")}</Text>
           </TouchableOpacity>
         </View>
       </View>
