d2616b93e8a3e7ff55f8d3a9fd83353320f41fc2
Dzenan Košuta <dzenan_kosuta@hotmail.com>
2024-06-04 15:12:48 +0200

    add content on profile tab

--- src/views/ProfileTab/ProfileTab.js
+++ src/views/ProfileTab/ProfileTab.js
@@ -1,22 +1,74 @@
-import { View, ScrollView, Linking, Alert } from "react-native";
-import React from "react";
+import { View, Text, Image } from "react-native";
+import React, { useState } from "react";
 import styles from "./ProfileTabStyles";
 import { useTranslation } from "react-i18next";
+import { useTheme } from "@react-navigation/native";
+import { CustomButton } from "../../components/CustomButton/CustomButton";
+import ConfirmModal from "../../components/Modals/ConfirmModal";
 
 const ProfileTab = () => {
+  const { colors } = useTheme();
   const { t } = useTranslation();
+  const [showModal, setShowModal] = useState(false);
 
-  const handleSite = async (url) => {
-    const supported = await Linking.canOpenURL(url);
-
-    if (supported) {
-      await Linking.openURL(url);
-    } else {
-      Alert(`${t("error_opening_site")} ${url}`);
-    }
-  };
-
-  return <ScrollView style={[styles.container, {}]}></ScrollView>;
+  return (
+    <View style={styles.container}>
+      <ConfirmModal
+        modalVisible={showModal}
+        setModalVisible={setShowModal}
+        title={t("delete_profile_title")}
+        info={t("delete_profile_info")}
+        confirm={() => {
+          console.log("odee");
+        }}
+      />
+      <View style={[styles.showProfile, {}]}>
+        <View style={[styles.borderName, { borderColor: colors.primary }]}>
+          <Text style={[styles.textInitials, { color: colors.primary }]}>
+            AS
+          </Text>
+        </View>
+        <View style={styles.profileWrapper}>
+          <View style={[styles.profileTextWrapper]}>
+            <Text style={[styles.text, { color: colors.text }]}>
+              Anes Sefovic
+            </Text>
+            <Text style={[styles.text, { color: colors.text }]}>
+              anes@gmail.com
+            </Text>
+          </View>
+        </View>
+      </View>
+      <View style={[styles.backgroundWrapper]}>
+        <Image
+          source={require("../../../assets/Images/logo.png")}
+          style={[styles.background]}
+        />
+      </View>
+      <View style={styles.buttonWrapper}>
+        <View style={styles.button}>
+          <CustomButton
+            text={t("delete_profile")}
+            type={"primary"}
+            onPress={() => {
+              setShowModal(true);
+            }}
+          />
+        </View>
+        <View style={styles.button}>
+          <CustomButton
+            text={t("logout")}
+            type={"secondary"}
+            // onPress={() => {
+            //   AsyncStorage.removeItem("auth_token").then(() => {
+            //     dispatch(authSlice.actions.logout());
+            //   });
+            // }}
+          />
+        </View>
+      </View>
+    </View>
+  );
 };
 
 export default ProfileTab;
--- src/views/ProfileTab/ProfileTabStyles.js
+++ src/views/ProfileTab/ProfileTabStyles.js
@@ -4,6 +4,67 @@ import { moderateScale } from "react-native-size-matters";
 const StoriesTabStyles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "space-between",
+  },
+  showProfile: {
+    paddingTop: moderateScale(60, 0.2),
+    paddingBottom: moderateScale(10, 0.2),
+    paddingHorizontal: moderateScale(30, 0.2),
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+  borderName: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: moderateScale(80, 0.2),
+    height: moderateScale(80, 0.2),
+    borderWidth: 1,
+    borderRadius: moderateScale(15, 0.2),
+  },
+  textInitials: {
+    fontSize: moderateScale(32, 0.2),
+    fontFamily: "PopinsRegular",
+  },
+  profileWrapper: {
+    justifyContent: "center",
+    paddingHorizontal: moderateScale(35, 0.2),
+    alignItems: "center",
+  },
+  profileTextWrapper: {
+    justifyContent: "center",
+    gap: moderateScale(5, 0.2),
+    paddingLeft: moderateScale(10, 0.2),
+  },
+  text: {
+    fontSize: moderateScale(14, 0.2),
+    textAlign: "center",
+    fontFamily: "PopinsRegular",
+  },
+  backgroundWrapper: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  background: {
+    width: moderateScale(300, 0.2),
+    height: moderateScale(300, 0.2),
+    opacity: 0.5,
+  },
+  buttonWrapper: {
+    bottom: moderateScale(20, 0.2),
+    marginTop: moderateScale(20, 0.2),
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: moderateScale(30, 0.2),
+  },
+  button: {
+    width: moderateScale(150, 0.2),
+    height: moderateScale(60, 0.2),
+    borderRadius: moderateScale(10, 0.2),
+    justifyContent: "center",
+    alignItems: "center",
+    gap: moderateScale(5, 0.2),
   },
 });
 
