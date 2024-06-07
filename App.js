81423cea9ef7b19fbc4b6cfa595c1b1960db3f2c
Dzenan Košuta <dzenan_kosuta@hotmail.com>
2024-06-07 21:30:02 +0200

    new order in App.js

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import "react-native-gesture-handler";
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { View, SafeAreaView, ActivityIndicator } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { Provider, useSelector } from "react-redux";
+import { Provider, useDispatch, useSelector } from "react-redux";
 import { store } from "./store";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { StatusBar } from "expo-status-bar";
@@ -13,6 +13,8 @@ import Tabs from "./src/components/Tabs/Tabs";
 import useAppFonts from "./src/hooks/useAppFonts";
 import "react-native-get-random-values";
 import Auth from "./src/views/Auth/Auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { authSlice } from "./src/store/authSlice";
 
 const queryClient = new QueryClient();
 
@@ -39,20 +41,59 @@ const primaryTheme = {
 
 const NavigationComponent = () => {
   const { areFontsLoaded } = useAppFonts();
+  const dispatch = useDispatch();
   const { selectedTheme } = useSelector((state) => state.theme);
   const [appIsReady, setAppIsReady] = useState(false);
   const { id } = useSelector((state) => state.auth);
 
-  (async () => {
-    SplashScreen.preventAutoHideAsync();
+  useEffect(() => {
+    (async () => {
+      SplashScreen.preventAutoHideAsync();
 
-    setTimeout(async () => {
-      if (areFontsLoaded) {
+      setTimeout(async () => {
+        if (areFontsLoaded) {
+          setAppIsReady(true);
+          await SplashScreen.hideAsync();
+        }
+      }, 1000);
+    })();
+    (async () => {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        const userData = await AsyncStorage.getItem("userData");
+        const token = await AsyncStorage.getItem("auth_token");
+        if (userData) {
+          const transformedUserData = JSON.parse(userData);
+          dispatch(authSlice.actions.setId(transformedUserData.id));
+          dispatch(authSlice.actions.setEmail(transformedUserData.email));
+          dispatch(
+            authSlice.actions.setFirstName(transformedUserData.firstName)
+          );
+          dispatch(authSlice.actions.setLastName(transformedUserData.lastName));
+          dispatch(authSlice.actions.setToken(token));
+        }
+      } catch (e) {
+        console.warn(e);
+      } finally {
         setAppIsReady(true);
         await SplashScreen.hideAsync();
       }
-    }, 1000);
-  })();
+    })();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      // This tells the splash screen to hide immediately! If we call this after
+      // `setAppIsReady`, then we may see a blank screen while the app is
+      // loading its initial state and rendering its first pixels. So instead,
+      // we hide the splash screen once we know the root view has already
+      // performed layout.
+      await SplashScreen.hideAsync();
+      // setTimeout(async () => {
+      // await SplashScreen.hideAsync();
+      // }, 1000);
+    }
+  }, [appIsReady]);
 
   if (!appIsReady) {
     return (
@@ -68,16 +109,9 @@ const NavigationComponent = () => {
     );
   }
 
-  if (!id) {
-    return (
-      <NavigationContainer theme={selectedTheme}>
-        <Auth />
-      </NavigationContainer>
-    );
-  }
   return (
     <NavigationContainer theme={selectedTheme}>
-      <Tabs />
+      {id ? <Tabs /> : <Auth />}
     </NavigationContainer>
   );
 };
